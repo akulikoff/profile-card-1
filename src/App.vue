a301e7b82e0305c1615ed93334e5dd834d47f5ac
akulikoff <akulikoff86@gmail.com>
2024-05-22 01:23:25 +1000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import ProfileCard from "./components/ProfileCard.vue";
+const data = {
+  fullName: "Sara Dole",
+  position: "Front End Engineer @ Microsoft",
+  summary:
+    "I turn coffee into bugs which are fixed by someone else. Certified Stack Overflow and ChatGPT developer.",
+  imageUrl: "src/assets/img/profile.png",
+};
+</script>
+
+<template>
+  <ProfileCard v-bind="data" />
+</template>
+
+<style scoped></style>
